3ad9f5fb7a4f92efa0fab91de958f79900a28b15
hukang <1165411366@qq.com>
2018-06-05 10:50:53 +0000

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -1,9 +1,25 @@
 <?php
 
 use Faker\Generator as Faker;
+use \App\Models\User;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    
+    $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+    $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $faker->sentence(),
+        'body' => $faker->text(),
+        'user_id' => $faker->randomElement($user_ids),
+        'category_id' => $faker->randomElement($category_ids),
+        'excerpt' => $faker->sentence(),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Models\User::class)->times(10)->create();
+
+        $user = \App\Models\User::find(1);
+        $user->name = 'hk';
+        $user->password = bcrypt('123456');
+        $user->email = '1165411366@qq.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+    }
+}
